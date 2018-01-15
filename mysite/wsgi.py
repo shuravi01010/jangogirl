44dcfda46b2f0c4c156c0179654a7b2c45497f21
shuravi01010 <mail2surovi@gmail.com>
2018-01-15 13:21:47 +0600

    simple blog app

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#
+# application = get_wsgi_application()
+
 import os
+import sys
+
+path = os.path.expanduser('~/my-first-blog')
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
